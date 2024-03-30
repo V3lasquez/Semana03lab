b5a18ba623b40ae8fc9ebc08a1fa1e9f131ba63d
V3lasquez <noe.vc1782@gmail.com>
2024-03-30 12:52:09 -0500

    Agregar archivos de proyecto.

---
+++ Semana03lab/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Semana03lab
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string connectionString = "Data Source=DESKTOP-F093S42\\SQLEXPRESS;Initial Catalog=Semana03;User Id=userTecsup;Password=userTecsup03";
+            try
+            {
+                //Cadena de conexión
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+
+                //Comandos de TRANSACT SQL
+                SqlCommand command = new SqlCommand("SELECT * FROM Students", connection);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+                DataTable dataTable = new DataTable();
+
+                adapter.Fill(dataTable);
+                dgvDemo.ItemsSource = dataTable.DefaultView;
+
+                MessageBox.Show("conexion exitosa");
+
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                // Manejar cualquier excepción que pueda ocurrir durante la recuperación o llenado de datos
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+}
